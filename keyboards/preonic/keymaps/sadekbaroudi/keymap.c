2df850e335c0ee68639bb4f52a7d7aa43caf1d9a
Sadek Baroudi <sadekbaroudi@gmail.com>
2020-12-17 14:11:41 -0800

    Removing macro functionality, it's buggy

--- keyboards/preonic/keymaps/sadekbaroudi/keymap.c
+++ keyboards/preonic/keymaps/sadekbaroudi/keymap.c
@@ -54,8 +54,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   RGB_TOG,               BL_TOGG,               KC_NO,                 KC_NO,                 KC_NO,                 KC_NO,                 KC_NO,                 KC_NO,                 KC_NO,                 KC_NO,                 MU_MOD,                MU_TOG,
   RESET,                 KC_NO,                 LSFT(KC_PGDN),         LSFT(KC_UP),           LSFT(KC_PGUP),         KC_NO,                 KC_MPLY,               KC_F7,                 KC_F8,                 KC_F9,                 KC_F10,                KC_NO,
   KC_NO,                 LSFT(KC_HOME),         LSFT(KC_LEFT),         LSFT(KC_DOWN),         LSFT(KC_RGHT),         LSFT(KC_END),          KC_VOLU,               KC_F4,                 KC_F5,                 KC_F6,                 KC_F11,                KC_MFFD,
-  KC_NO,                 LCTL(KC_Z),            LCTL(KC_X),            LCTL(KC_C),            LCTL(KC_V),            DM_PLY1,               KC_VOLD,               KC_F1,                 KC_F2,                 KC_F3,                 KC_F12,                KC_MRWD,
-  KC_NO,                 KC_NO,                 KC_NO,                 KC_NO,                 KC_TRNS,               DM_REC1,               DM_REC1,               KC_TRNS,               KC_NO,                 KC_NO,                 KC_NO,                 KC_NO
+  KC_NO,                 LCTL(KC_Z),            LCTL(KC_X),            LCTL(KC_C),            LCTL(KC_V),            KC_NO,                 KC_VOLD,               KC_F1,                 KC_F2,                 KC_F3,                 KC_F12,                KC_MRWD,
+  KC_NO,                 KC_NO,                 KC_NO,                 KC_NO,                 KC_TRNS,               KC_NO,                 KC_NO,                 KC_TRNS,               KC_NO,                 KC_NO,                 KC_NO,                 KC_NO
 )
 
 };
