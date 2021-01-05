566d67a21f23eef9d810d0faa498d91db73d748c
Sadek Baroudi <sadekbaroudi@gmail.com>
2021-01-05 09:31:56 -0800

    updated corners, may revisit

--- keyboards/reviung39/keymaps/sadekbaroudi/keymap.c
+++ keyboards/reviung39/keymaps/sadekbaroudi/keymap.c
@@ -222,25 +222,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
 // SAWDMAKdh-2
 [1] = LAYOUT_reviung39(
-  KC_ESC,                KC_Q,                  KC_W,                  KC_F,                  KC_R,                  KC_J,                  KC_Y,                  KC_U,                  KC_I,                  KC_O,                  KC_P,                  KC_BSLS,
+  KC_ESC,                KC_Q,                  KC_W,                  KC_F,                  KC_R,                  KC_K,                  KC_Y,                  KC_U,                  KC_I,                  KC_O,                  KC_P,                  KC_BSLS,
   KC_TAB,                LCTL_T(KC_A),          LGUI_T(KC_S),          LALT_T(KC_D),          LSFT_T(KC_T),          KC_G,                  KC_H,                  RSFT_T(KC_N),          RALT_T(KC_E),          RGUI_T(KC_L),          RCTL_T(KC_SCLN),       LT(6,KC_QUOT),
-  KC_DEL,                KC_Z,                  KC_X,                  KC_C,                  KC_V,                  KC_B,                  KC_K,                  KC_M,                  KC_COMM,               KC_DOT,                KC_SLSH,               KC_BSPC,
+  KC_DEL,                KC_Z,                  KC_X,                  KC_C,                  KC_V,                  KC_B,                  KC_J,                  KC_M,                  KC_COMM,               KC_DOT,                KC_SLSH,               KC_BSPC,
                                                                                               LT(2,KC_ENT),                  LT(4,KC_BSPC),                        LT(3,KC_SPC)
 ),
 
 // SAWDMAKdh-3
 [1] = LAYOUT_reviung39(
-  KC_ESC,                KC_Q,                  KC_W,                  KC_F,                  KC_P,                  KC_V,                  KC_J,                  KC_U,                  KC_I,                  KC_Y,                  KC_SCLN,               KC_BSLS,
+  KC_ESC,                KC_Q,                  KC_W,                  KC_F,                  KC_P,                  KC_K,                  KC_J,                  KC_U,                  KC_I,                  KC_Y,                  KC_SCLN,               KC_BSLS,
   KC_TAB,                LCTL_T(KC_A),          LGUI_T(KC_R),          LALT_T(KC_S),          LSFT_T(KC_T),          KC_G,                  KC_H,                  RSFT_T(KC_N),          RALT_T(KC_E),          RGUI_T(KC_L),          RCTL_T(KC_O),          LT(6,KC_QUOT),
-  KC_DEL,                KC_Z,                  KC_X,                  KC_C,                  KC_D,                  KC_B,                  KC_K,                  KC_M,                  KC_COMM,               KC_DOT,                KC_SLSH,               KC_BSPC,
+  KC_DEL,                KC_Z,                  KC_X,                  KC_C,                  KC_D,                  KC_B,                  KC_V,                  KC_M,                  KC_COMM,               KC_DOT,                KC_SLSH,               KC_BSPC,
                                                                                               LT(2,KC_ENT),                  LT(4,KC_BSPC),                        LT(3,KC_SPC)
 ),
 
-// COLEMAK-DH-modBV
+// COLEMAK-DH-modCorners
 [1] = LAYOUT_reviung39(
-  KC_ESC,                KC_Q,                  KC_W,                  KC_F,                  KC_P,                  KC_V,                  KC_J,                  KC_L,                  KC_U,                  KC_Y,                  KC_SCLN,               KC_BSLS,
+  KC_ESC,                KC_Q,                  KC_W,                  KC_F,                  KC_P,                  KC_K,                  KC_J,                  KC_L,                  KC_U,                  KC_Y,                  KC_SCLN,               KC_BSLS,
   KC_TAB,                LCTL_T(KC_A),          LGUI_T(KC_R),          LALT_T(KC_S),          LSFT_T(KC_T),          KC_G,                  KC_M,                  RSFT_T(KC_N),          RALT_T(KC_E),          RGUI_T(KC_I),          RCTL_T(KC_O),          LT(6,KC_QUOT),
-  KC_DEL,                KC_Z,                  KC_X,                  KC_C,                  KC_D,                  KC_B,                  KC_K,                  KC_H,                  KC_COMM,               KC_DOT,                KC_SLSH,               KC_BSPC,
+  KC_DEL,                KC_Z,                  KC_X,                  KC_C,                  KC_D,                  KC_B,                  KC_V,                  KC_H,                  KC_COMM,               KC_DOT,                KC_SLSH,               KC_BSPC,
                                                                                               LT(2,KC_ENT),                  LT(4,KC_BSPC),                        LT(3,KC_SPC)
 ),
 */
