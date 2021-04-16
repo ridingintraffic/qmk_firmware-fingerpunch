f3e8ae1e06e8fb000b7811092c662e2d5f2597bf
Sadek Baroudi <sadekbaroudi@gmail.com>
2021-04-16 11:36:48 -0700

    Adding draculad keyboard

--- keyboards/draculad/rules.mk
+++ keyboards/draculad/rules.mk
@@ -1,24 +1,28 @@
 # MCU name
 MCU = atmega32u4
 
+
+EXTRAFLAGS += -flto
 # Bootloader selection
 BOOTLOADER = caterina
 
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = yes        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+UNICODE_ENABLE = no        # Unicode
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-SPLIT_KEYBOARD = yes
-OLED_ENABLE = yes
-OLED_DRIVER = SSD1306
+SPLIT_KEYBOARD = yes        # Use shared split_common code
+
+OLED_DRIVER_ENABLE = yes
 WPM_ENABLE = yes
 ENCODER_ENABLE = yes
-LTO_ENABLE = yes
+RGBLIGHT_ENABLE = yes 
+
+OLED_DRIVER_ENABLE = yes
