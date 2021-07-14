01020b3718ac4285162410dd555b81e107e156a5
Sadek Baroudi <sadekbaroudi@gmail.com>
2021-07-14 02:21:40 -0700

    adding handwired barobord

---
+++ keyboards/handwired/barobordhw/barobordhw.h
@@ -0,0 +1,39 @@
+/* Copyright 2021 Sadek Baroudi <sadekbaroudi@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "quantum.h"
+
+/* This a shortcut to help you visually see your layout.
+ *
+ * The first section contains all of the arguments representing the physical
+ * layout of the board and position of the keys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
+#define LAYOUT_barobordhw( \
+    K00, K01, K02, K03, K04,      K09, K08, K07, K06, K05, \
+    K10, K11, K12, K13, K14,      K19, K18, K17, K16, K15, \
+    K20, K21, K22, K23, K24,      K29, K28, K27, K26, K25, \
+    K30, K31, K32, K33, K34,      K39, K38, K37, K36, K35 \
+) \
+{ \
+    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09 }, \
+    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19 }, \
+    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29 }, \
+    { K30, K31, K32, K33, K34, K35, K36, K37, K38, K39 } \
+}
---
+++ keyboards/handwired/barobordhw/readme.md
@@ -0,0 +1,16 @@
+# barobordhw
+
+An ergonomic 30% unibody dactyl like keyboard
+
+* Keyboard Maintainer: [sadekbaroudi](https://github.com/sadekbaroudi)
+* Hardware Supported: Pro Micro 5V/16MHz and compatible.
+
+Make example for this keyboard (after setting up your build environment):
+
+    make handwired/barobordhw:default
+
+Example of flashing this keyboard:
+
+    make barobordhw:default:flash
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
