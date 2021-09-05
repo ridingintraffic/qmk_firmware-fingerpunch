60f99ea5f1fe75b417837e96b29ab2febfff99d4
Sadek Baroudi <sadekbaroudi@gmail.com>
2021-09-05 00:22:01 -0700

    forgot this in the last commit

--- keyboards/xoiviox/barobord/rules-rgb-matrix.mk
+++ keyboards/xoiviox/barobord/rules-rgb-matrix.mk
@@ -34,7 +34,7 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-#ENCODER_ENABLE = yes
+ENCODER_ENABLE = yes
 
 # If you don't have pimoroni trackball
 #OLED_DRIVER_ENABLE = yes # this can be yes or no depending on if you have an OLED
