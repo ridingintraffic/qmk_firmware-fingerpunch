1984c5052d18dab39b7adf3258c7e9250184a762
Sadek Baroudi <sadekbaroudi@gmail.com>
2021-09-10 01:56:00 -0700

    minidox keyboard keymap

---
+++ bin/build-minidox.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# TODO: consolidate all the build files into one
+
+#cp keyboards/xoiviox/barobord/rules-oled.mk keyboards/xoiviox/barobord/rules.mk
+make minidox/rev1:sadekbaroudi ; cp .build/minidox_rev1_sadekbaroudi* /mnt/c/Users/sadek/Google\ Drive/qmk-keyboards/minidox/ ;
+#rm keyboards/xoiviox/barobord/rules.mk
