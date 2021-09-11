05ad66ba1678071b54c900591f0e2867a12206c7
Sadek Baroudi <sadekbaroudi@gmail.com>
2021-09-11 01:28:24 -0700

    added my adux keymap

---
+++ bin/build-adux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# TODO: consolidate all the build files into one
+
+#cp keyboards/xoiviox/barobord/rules-oled.mk keyboards/xoiviox/barobord/rules.mk
+make a_dux:sadekbaroudi ; cp .build/a_dux_sadekbaroudi* /mnt/c/Users/sadek/Google\ Drive/qmk-keyboards/a_dux/ ;
+#rm keyboards/xoiviox/barobord/rules.mk
---
+++ keyboards/a_dux/keymaps/sadekbaroudi/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define MASTER_LEFT
+
+#define USE_SERIAL
