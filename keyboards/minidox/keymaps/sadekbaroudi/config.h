daffa79a3479204239fbc046204e51487e748a3a
Sadek Baroudi <sadekbaroudi@gmail.com>
2021-09-12 01:11:06 -0700

    added minidox rgb underglow support

--- keyboards/minidox/keymaps/sadekbaroudi/config.h
+++ keyboards/minidox/keymaps/sadekbaroudi/config.h
@@ -3,3 +3,12 @@
 #define MASTER_LEFT
 
 #define USE_SERIAL
+
+#define RGB_DI_PIN B3
+
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLED_NUM 18 // Number of LEDs
+#    define RGBLED_SPLIT \
+        { 9, 9 }
+#    define RGBLIGHT_SPLIT
+#endif
