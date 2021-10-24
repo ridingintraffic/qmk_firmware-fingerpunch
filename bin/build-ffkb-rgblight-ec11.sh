d0e434b27dab934aa32203c70f83b2ef636b734a
Sadek Baroudi <sadekbaroudi@gmail.com>
2021-10-24 02:54:17 -0700

    add support for ffkb with all 3 ec11 encoders

---
+++ bin/build-ffkb-rgblight-ec11.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# TODO: consolidate all the build files into one
+
+cp keyboards/xoiviox/ffkb/config.h keyboards/xoiviox/ffkb/config.h.bak
+cp keyboards/xoiviox/ffkb/rules.mk keyboards/xoiviox/ffkb/rules.mk.bak
+cp keyboards/xoiviox/ffkb/config-rgblight-ec11.h keyboards/xoiviox/ffkb/config.h
+cp keyboards/xoiviox/ffkb/rules-rgblight-ec11.mk keyboards/xoiviox/ffkb/rules.mk
+make xoiviox/ffkb:sadekbaroudi
+mv keyboards/xoiviox/ffkb/config.h.bak keyboards/xoiviox/ffkb/config.h
+mv keyboards/xoiviox/ffkb/rules.mk.bak keyboards/xoiviox/ffkb/rules.mk
+cp .build/xoiviox_ffkb_sadekbaroudi* /mnt/g/My\ Drive/qmk-keyboards/ffkb-rgblight-ec11/
