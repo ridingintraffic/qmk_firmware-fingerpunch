9739461d5a54a70bdd5778bfe7bb26317115524b
Sadek Baroudi <sadekbaroudi@gmail.com>
2021-12-08 00:55:26 -0800

    updated encoders

--- users/sadekbaroudi/encoder_stuff.c
+++ users/sadekbaroudi/encoder_stuff.c
@@ -9,65 +9,127 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 
     // default behavior if undefined
     if (index == 0) {
-        // Conditional to reverse the direction of encoder number 1
-        // The reason I have this is that for some of my boards, it supports two different types of encoders, and they may differ in direction
-        #ifdef ENCODERS_A_REVERSE
-        if (!clockwise) {
-        #else
-        if (clockwise) {
-        #endif
-            #if defined(RGBLIGHT_ENABLE) && defined(ENCODERS_FOR_RGB)
-            rgblight_increase_hue();
-            #else
-            tap_code(KC_VOLU);
-            #endif
-        } else {
-            #if defined(RGBLIGHT_ENABLE) && defined(ENCODERS_FOR_RGB)
-            rgblight_decrease_hue();
-            #else
-            tap_code(KC_VOLD);
-            #endif
+        switch(biton32(layer_state)){
+            case _SYMBOLS:
+                // Page up / page down
+                if (clockwise) {
+                    #ifdef ENCODERS_A_REVERSE
+                    tap_code(KC_PGUP);
+                    #else
+                    tap_code(KC_PGDN);
+                    #endif
+                } else {
+                    #ifdef ENCODERS_A_REVERSE
+                    tap_code(KC_PGDN);
+                    #else
+                    tap_code(KC_PGUP);
+                    #endif
+                }
+                break;
+            case _MEDIA:
+                // zoom in and out
+                if (clockwise) {
+                    #ifdef ENCODERS_A_REVERSE
+                    tap_code16(C(KC_MINS));
+                    #else
+                    tap_code16(C(S(KC_EQL)));
+                    #endif
+                } else {
+                    #ifdef ENCODERS_A_REVERSE
+                    tap_code16(C(S(KC_EQL)));
+                    #else
+                    tap_code16(C(KC_MINS));
+                    #endif
+                }
+                break;
+            default:
+                // volume up and down
+                if (clockwise){
+                    #ifdef ENCODERS_A_REVERSE
+                    tap_code(KC_VOLD);
+                    #else
+                    tap_code(KC_VOLU);
+                    #endif
+                } else{
+                    #ifdef ENCODERS_A_REVERSE
+                    tap_code(KC_VOLU);
+                    #else
+                    tap_code(KC_VOLD);
+                    #endif
+                }
+                break;
         }
-
-        #ifdef RGBLIGHT_ENABLE
-        rgb_set_user_config_from_current_values();
-        #endif
-    }
-    else if (index == 1) {
-      // Conditional to reverse the direction of encoder number 1
-      // The reason I have this is that for some of my boards, it supports two different types of encoders, and they may differ in direction
-      #ifdef ENCODERS_B_REVERSE
-      if (!clockwise) {
-      #else
-      if (clockwise) {
-      #endif
-        #if defined(RGBLIGHT_ENABLE) && defined(ENCODERS_FOR_RGB)
-        rgblight_step();
-        #else
-        tap_code16(C(KC_RGHT));
-        #endif
-      }
-      else{
-        #if defined(RGBLIGHT_ENABLE) && defined(ENCODERS_FOR_RGB)
-        rgblight_step_reverse();
-        #else
-        tap_code16(C(KC_LEFT));
-        #endif
-      }
-      #if defined(RGBLIGHT_ENABLE) && defined(ENCODERS_FOR_RGB)
-      rgb_set_user_config_from_current_values();
-      #endif
+    } else if (index == 1) {
+        switch(biton32(layer_state)){
+            case _NAVIGATION:
+                // word select left and right
+                if (clockwise) {
+                    #ifdef ENCODERS_B_REVERSE
+                    tap_code16(C(S(KC_LEFT)));
+                    #else
+                    tap_code16(C(S(KC_RIGHT)));
+                    #endif
+                } else {
+                    #ifdef ENCODERS_B_REVERSE
+                    tap_code16(C(S(KC_RIGHT)));
+                    #else
+                    tap_code16(C(S(KC_LEFT)));
+                    #endif
+                }
+                break;
+            case _FUNCTION:
+                // super alt tab 
+                if (clockwise) {
+                    #ifdef ENCODERS_B_REVERSE
+                    press_super_alt_tab(true);
+                    #else
+                    press_super_alt_tab(false);
+                    #endif
+                } else {
+                    #ifdef ENCODERS_B_REVERSE
+                    press_super_alt_tab(false);
+                    #else
+                    press_super_alt_tab(true);
+                    #endif
+                }
+                break;
+            default:
+                // cursor move one word at a time, left and right
+                if (clockwise) {
+                    #ifdef ENCODERS_B_REVERSE
+                    tap_code16(C(KC_LEFT));
+                    #else
+                    tap_code16(C(KC_RIGHT));
+                    #endif
+                } else {
+                    #ifdef ENCODERS_B_REVERSE
+                    tap_code16(C(KC_RIGHT));
+                    #else
+                    tap_code16(C(KC_LEFT));
+                    #endif
+                }
+                break;
+        }
     }
     else if (index == 2) {
-      #ifdef ENCODERS_C_REVERSE
-      if (!clockwise) {
-      #else
-      if (clockwise) {
-      #endif
-        press_super_alt_tab(true);
-      } else {
-        press_super_alt_tab(false);
-      }
+        switch(biton32(layer_state)){
+            default:
+                // scroll browser tab left and right
+                if (clockwise){
+                    #ifdef ENCODERS_B_REVERSE
+                    tap_code16(C(S(KC_TAB)));
+                    #else
+                    tap_code16(C(KC_TAB));
+                    #endif
+                } else{
+                    #ifdef ENCODERS_B_REVERSE
+                    tap_code16(C(KC_TAB));
+                    #else
+                    tap_code16(C(S(KC_TAB)));
+                    #endif
+                }
+                break;
+        }
     }
 
     return true;
