2645aad0c770a8e78e108e12f2598c4355242e14
Sadek Baroudi <sadekbaroudi@gmail.com>
2022-01-10 03:06:33 -0800

    added rgbmatrix support

--- bin/build-pinkies-out.sh
+++ bin/build-pinkies-out.sh
@@ -2,8 +2,11 @@
 
 # TODO: consolidate all the build files into one
 
-make xoiviox/pinkiesout/rgblight_oled:default
-cp .build/xoiviox_pinkiesout_rgblight_oled_default* /mnt/g/My\ Drive/qmk-keyboards/pinkiesout
+make xoiviox/pinkiesout/rgbmatrix_oled:default
+cp .build/xoiviox_pinkiesout_rgbmatrix_oled_default* /mnt/g/My\ Drive/qmk-keyboards/pinkiesout
+
+#make xoiviox/pinkiesout/rgblight_oled:sadekbaroudi
+#cp .build/xoiviox_pinkiesout_rgblight_oled_sadekbaroudi* /mnt/g/My\ Drive/qmk-keyboards/pinkiesout/rgblight_oled/
 
 #make xoiviox/pinkiesout/rgbmatrix_oled:sadekbaroudi
-#cp .build/xoiviox_pinkiesout_rgblight_oled_sadekbaroudi* /mnt/g/My\ Drive/qmk-keyboards/pinkiesout
+#cp .build/xoiviox_pinkiesout_rgbmatrix_oled_sadekbaroudi* /mnt/g/My\ Drive/qmk-keyboards/pinkiesout/rgbmatrix/
