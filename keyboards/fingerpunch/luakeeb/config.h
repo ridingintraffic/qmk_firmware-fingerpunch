cb858c207675b9d0aae283e674435bdd876f35f4
Sadek Baroudi <sadekbaroudi@gmail.com>
2022-02-15 02:50:04 -0800

    increase max led brightness for luakeeb

--- keyboards/fingerpunch/luakeeb/config.h
+++ keyboards/fingerpunch/luakeeb/config.h
@@ -54,7 +54,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_HUE_STEP 16
   #define RGBLIGHT_SAT_STEP 16
   #define RGBLIGHT_VAL_STEP 16
-  #define RGBLIGHT_LIMIT_VAL 60 /* The maximum brightness level for RGBLIGHT_ENABLE */
+  #define RGBLIGHT_LIMIT_VAL 120 /* The maximum brightness level for RGBLIGHT_ENABLE */
   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
 // /*== all animations enable ==*/
 //  #define RGBLIGHT_ANIMATIONS
