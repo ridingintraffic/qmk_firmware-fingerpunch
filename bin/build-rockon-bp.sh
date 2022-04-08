8a2eadc6b8a1cf37d4ffb68123fda425a18ec92a
Sadek Baroudi <sadekbaroudi@gmail.com>
2022-04-08 13:49:20 -0700

    fixed build script for rock on blackpill

--- bin/build-rockon-bp.sh
+++ bin/build-rockon-bp.sh
@@ -3,12 +3,12 @@
 # TODO: consolidate all the build files into one
 
 make fingerpunch/rockon_bp/rgbmatrix_oled:sadekbaroudi
-cp .build/fingerpunch_rockon_bp_rgbmatrix_oled_sadekbaroudi.bin /mnt/g/My\ Drive/qmk-keyboards/rock-on/rgbmatrix/fingerpunch_rockon_bp_rgbmatrix_oled_sadekbaroudi.bin
+cp .build/fingerpunch_rockon_bp_rgbmatrix_oled_sadekbaroudi.bin /mnt/g/My\ Drive/qmk-keyboards/rock-on_bp/sadekbaroudi/fingerpunch_rockon_bp_rgbmatrix_oled_sadekbaroudi.bin
 
 make fingerpunch/rockon_bp/rgblight_oled:sadekbaroudi
-cp .build/fingerpunch_rockon_bp_rgblight_oled_sadekbaroudi.bin /mnt/g/My\ Drive/qmk-keyboards/rock-on/rgblight/fingerpunch_rockon_bp_rgblight_oled_sadekbaroudi.bin
+cp .build/fingerpunch_rockon_bp_rgblight_oled_sadekbaroudi.bin /mnt/g/My\ Drive/qmk-keyboards/rock-on_bp/sadekbaroudi/fingerpunch_rockon_bp_rgblight_oled_sadekbaroudi.bin
 
 make fingerpunch/rockon_bp/rgbmatrix:sadekbaroudi
-cp .build/fingerpunch_rockon_bp_rgbmatrix_sadekbaroudi.bin /mnt/g/My\ Drive/qmk-keyboards/rock-on/rgbmatrix/fingerpunch_rockon_bp_rgbmatrix_sadekbaroudi.bin
+cp .build/fingerpunch_rockon_bp_rgbmatrix_sadekbaroudi.bin /mnt/g/My\ Drive/qmk-keyboards/rock-on_bp/sadekbaroudi/fingerpunch_rockon_bp_rgbmatrix_sadekbaroudi.bin
 
 
