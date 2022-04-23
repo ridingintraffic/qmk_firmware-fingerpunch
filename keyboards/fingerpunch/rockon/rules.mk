da3db2cbf33e04b7c680a7e6a1e60cc1e64cfbb6
Sadek Baroudi <sadekbaroudi@gmail.com>
2022-04-23 00:31:24 -0700

    forgot to include this in a previous commit

--- keyboards/fingerpunch/rockon/rules.mk
+++ keyboards/fingerpunch/rockon/rules.mk
@@ -38,12 +38,3 @@ ENCODER_ENABLE = no
 OLED_ENABLE = yes
 # EXTRAFLAGS     += -flto     # macros disabled, if you need the extra space
 MOUSEKEY_ENABLE = no
-
-PIMORONI_TRACKBALL_ENABLE = no
-
-ifeq ($(strip $(PIMORONI_TRACKBALL_ENABLE)), yes)
-    POINTING_DEVICE_ENABLE := yes
-    SRC += drivers/sensors/pimoroni_trackball.c
-    QUANTUM_LIB_SRC += i2c_master.c
-    OPT_DEFS += -DPIMORONI_TRACKBALL_ENABLE
-endif
