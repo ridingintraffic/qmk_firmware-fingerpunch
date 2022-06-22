7bbfba97e506b217e4ba307f3555af0f72d9d5b3
Sadek Baroudi <sadekbaroudi@gmail.com>
2022-06-22 01:12:48 -0700

    updated first column in ffkb sadekbaroudi keymap to use mouse clicks for cirque

--- keyboards/fingerpunch/ffkb_byomcu/keymaps/sadekbaroudi/keymap.c
+++ keyboards/fingerpunch/ffkb_byomcu/keymaps/sadekbaroudi/keymap.c
@@ -38,9 +38,9 @@
               K33, K34, K35, K36, K37, K38 \
   ) \
   LAYOUT_wrapper( \
-        N_SEL_LINE,      K01,            K02,            K03,            LT(_FUNCTION, K04),     K05,                      K06,                   LT(_FUNCTION, K07),    K08,              K09,            K0A,          KC_BSLS, \
-        LCTL(KC_C),      LCTL_T(K11),    LGUI_T(K12),    LALT_T(K13),    LSFT_T(K14),            K15,                      LT(_MOUSE, K16),       RSFT_T(K17),           RALT_T(K18),      RGUI_T(K19),    RCTL_T(K1A),  LCTL(KC_V), \
-        TG(_NAVIGATION), K21,            K22,            K23,            K24,                    K25,                      K26,                   K27,                   K28,              K29,            K2A,          S_ALT_TAB, \
+        KC_MS_BTN3,      K01,            K02,            K03,            LT(_FUNCTION, K04),     K05,                      K06,                   LT(_FUNCTION, K07),    K08,              K09,            K0A,          KC_BSLS, \
+        KC_MS_BTN1,      LCTL_T(K11),    LGUI_T(K12),    LALT_T(K13),    LSFT_T(K14),            K15,                      LT(_MOUSE, K16),       RSFT_T(K17),           RALT_T(K18),      RGUI_T(K19),    RCTL_T(K1A),  LCTL(KC_V), \
+        KC_MS_BTN2,      K21,            K22,            K23,            K24,                    K25,                      K26,                   K27,                   K28,              K29,            K2A,          S_ALT_TAB, \
                                  KC_MUTE,                K33,            LT(_NAVIGATION,K34),    LT(_FUNCTION,K35),        LT(_MEDIA,K36),        LT(_SYMBOLS,K37),      K38,              LCTL(KC_BSPC), \
                                                                                                            N_DEL_LINE \
     )
